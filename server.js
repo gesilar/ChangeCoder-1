33c62dfbd5b0dc7bfab91e86cdc73bcc70a59471
鲍剑 <jian_bao@outlook.com>
2017-02-07 23:11:13 +0800

    add ico

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var path=require('path');
 
 var express=require('express');
+var favicon = require('express-favicon');
 
 //自定义模块
 var web_api=require('./src/routes/web_api');
@@ -15,6 +16,8 @@ app.engine('html',require('ejs').__express);
 app.set('view engine','html');
 //设置视图
 app.set('views',path.join(__dirname,'views'));
+//设置网站图标
+app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
 //注册路由web_api,logic_api并使用
 app.use('/',web_api);
 app.set('port', process.env.PORT || 8080);
