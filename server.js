16b508046245ee23002efa6f44b6da4544a1350f
鲍剑 <jian_bao@outlook.com>
2017-02-07 09:48:23 +0800

    remove others

--- server.js
+++ server.js
@@ -3,29 +3,11 @@ const Hapi = require("hapi");
 const server = new Hapi.Server();
 
 const staticRoute = require("./src/routes/static-files");
-// const HapiReactview = require("./src/templates/utils/hapi-react-view");
 
 server.connection({
     port: 8080,
     host: "localhost"
 });
-
-// server.register(require("vision"), (err) => {
-//     if (err) {
-//         throw err;
-//     }
-
-//     server.views({
-//         defaultExtension: "js",
-//         engines: {
-//             jsx: HapiReactview,
-//             js: HapiReactview
-//         },
-//         compileOptions: {},
-//         relativeTo: __dirname,
-//         path: "src/templates"
-//     });
-// });
 
 server.register(require("inert"), (err) => {
     if (err) {
