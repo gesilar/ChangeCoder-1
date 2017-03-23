46682fb25164aa1294918d99e9529ad88e4c456a
鲍剑 <jian_bao@outlook.com>
2017-03-24 00:19:09 +0800

    change

--- tools/spider.js
+++ tools/spider.js
@@ -6,35 +6,66 @@ const iconv = require("iconv-lite");
 
 const config = require("../config");
 
-const startCatch = () => {
+const startCatch = (article) => {
     const db =mongoose.createConnection(config.connectUrl);
+    
     db.on('err', () => {
         console.log(`connect error, info: ${err}`);
     });
     db.on('open', () => {
         console.log(`connected successfully`);
-        startPost(db);
+        SaveArticle(article, db);
     })
 };
 
 const SaveArticle = (article, db) => {
     const ArticleScheme = new mongoose.Schema({
         name: String,
-        sourceUrl: String,
-        chapterList: []
+        sourceUrl: String
     });
     const ArticleModel = db.model('Article', ArticleScheme);
+    const ChapterScheme = new mongoose.Schema({
+        name: String,
+        sourceUrl: String,
+        content: String,
+        articleId: mongoose.Schema.ObjectId
+    });
+    const ChapterModel = db.model('Chapter', ChapterScheme);
     const articleEntity = new ArticleModel({
         name: article.name,
-        sourceUrl: article.sourceUrl,
-        chapterList: article.chapterList
+        sourceUrl: article.sourceUrl
     });
     articleEntity.save().then(function(doc){
-        console.log(`${doc.name} has been saved`);
+        if (doc) {
+            console.log(`${doc.name} has been saved`);
+            asyncEachSeries(article.chapterList, doc._id, db, ChapterModel);
+        } else {
+            console.log(`save error`);
+        }
+        
     });
 };
 
-const startPost = (db) => {
+const SaveChapter = (chapter, articleId, db, ChapterModel, callback) => {
+    const chapterEntity = new ChapterModel({
+        name: chapter.name,
+        sourceUrl: chapter.sourceUrl,
+        content: chapter.content,
+        articleId: articleId
+    });
+
+    chapterEntity.save().then(function(doc){
+        if (doc) {
+            callback(null);
+            console.log(`${doc.name} has been saved`);
+        } else {
+            callback(false);
+            console.log(`save error`);
+        }
+    })
+};
+
+const startPost = () => {
     const article = {name: "霸天战皇", sourceUrl: "http://www.boluoxs.com/biquge/9/9885/"};
     http.get('http://www.boluoxs.com/biquge/9/9885/', (res) => {
         let html = '';
@@ -52,28 +83,48 @@ const startPost = (db) => {
                 option.url = 'http://www.boluoxs.com/biquge/9/9885/' + chapter.find('a').attr('href');
                 article.chapterList.push(option);
             });
-            asyncAction(article, db);
+            console.log(article.chapterList.length);
+            //asyncAction(article);
+            asyncParallelLimit(article);
         });
     }).on('error', (e) => {
-        console.log(`错误: ${e.message}`);
+        console.log(`startPost错误: ${e}`);
     });
 }
 
-const asyncAction = (article, db) => {
-    async.eachSeries(article.chapterList, (item, callback) => {
-        setTimeout(saveChapter, 100, item, callback);
+const asyncEachSeries = (chapterList, articleId, db, ChapterModel) => {
+    console.log(`章结数为:${chapterList.length}`);
+    async.eachSeries(chapterList, (item, done) => {
+        SaveChapter(item, articleId, db, ChapterModel, done);
     }, (err, result) => {
         if (err) {
             console.log(`错误: ${e.message}`);
-        } else {
-            SaveArticle(article, db);
-        }
+        } 
+        console.log(`save success`);
     });
 }
 
-const saveChapter = (chapter, callback) => {
-    console.log(chapter.url);
-    http.get(chapter.url, (res) => {
+const asyncParallelLimit = (article) => {
+    const task = article.chapterList.map((item, index) => {
+        return function(callback) {
+            getChapterContent(item, callback);
+        }
+    });
+    console.time('parallel');
+    async.parallelLimit(task, 100, (err, results) => {
+        if (err) {
+           console.log(`asyncParallel错误: ${err}`); 
+        } else {
+            article.chapterList = results;
+            console.log(`get article chapterlist length : ${results.length}`);
+            startCatch(article);
+        }
+        console.timeEnd("parallel");  
+    });
+}
+
+const getChapterContent = (chapter, callback) => {
+        http.get(chapter.url, (res) => {
         if (res.statusCode === 200) {
             let rawData = '';
             res.on('data', (data) => {
@@ -84,13 +135,13 @@ const saveChapter = (chapter, callback) => {
                 const content = $('#book_text');
                 chapter.content = content.html();
                 console.log(`${chapter.name} is OK`);
-                callback(null);
+                callback(null, chapter);
             });
         }
     }).on('error', (e) => {
-        console.log(`错误: ${e.message}`);
-        callback(false);
+        console.log(`${chapter.name}:${chapter.url}getContent错误: ${e}`);
+        callback(e, null);
     });
-};
+}
 
-startCatch();
+startPost();
